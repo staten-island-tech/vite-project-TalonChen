84a4cbf6eea3a139b95ee4800e0a83781f2ebf65
TalonChen <talonc@nycstudents.net>
2025-11-13 11:00:57 -0800

    a

--- app/src/main.js
+++ app/src/main.js
@@ -1,2 +1,109 @@
 import "./style.css";
 //import { setupCounter } from "./counter.js";
+
+//Steps to Success :)
+//Array of Music
+//Create different categories of the music - In HTML
+
+const music = [
+  {
+    name: "Megalovania",
+    artist: "Toby Fox",
+    genre: "game",
+  },
+  {
+    name: "Clash Royale Theme Song",
+    artist: "Carl V",
+    genre: "game",
+  },
+  {
+    name: "Pigstep",
+    artist: "Lena Raine",
+    genre: "game",
+  },
+  {
+    name: "Jumper",
+    artist: "Waterflame",
+    genre: "game",
+  },
+  {
+    name: "Lifelight",
+    artist: "Hideki Sakamoto",
+    genre: "game",
+  },
+  {
+    name: "Blinding Lights",
+    artist: "The Weeknd",
+    genre: "pop",
+  },
+  {
+    name: "Shape of You",
+    artist: "Ed Sheeran",
+    genre: "pop",
+  },
+  {
+    name: "Treat You Better",
+    artist: "Shawn Mendes",
+    genre: "pop",
+  },
+  {
+    name: "",
+    artist: "",
+    genre: "",
+  },
+  {
+    name: "",
+    artist: "",
+    genre: "",
+  },
+  {
+    name: "",
+    artist: "",
+    genre: "",
+  },
+  {
+    name: "",
+    artist: "",
+    genre: "",
+  },
+  {
+    name: "",
+    artist: "",
+    genre: "",
+  },
+  {
+    name: "",
+    artist: "",
+    genre: "",
+  },
+  {
+    name: "",
+    artist: "",
+    genre: "",
+  },
+  {
+    name: "",
+    artist: "",
+    genre: "",
+  },
+  {
+    name: "Smells Like Teen Spirit",
+    artist: "Nirvana",
+    genre: "rock",
+  },
+  {
+    name: "Bohemian Rhapsody",
+    artist: "Queen",
+    genre: "rock",
+  },
+  {
+    name: "Hotel California",
+    artist: "Eagles",
+    genre: "rock",
+  },
+  {
+    name: "Back in Black",
+    artist: "AC/DC",
+    genre: "rock",
+  },
+];
