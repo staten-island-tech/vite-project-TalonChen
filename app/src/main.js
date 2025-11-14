8d3c65976b3b0eff4824347cc1d410983845cecd
TalonChen <talonc@nycstudents.net>
2025-11-14 10:54:42 -0800

    aeeefdae

--- app/src/main.js
+++ app/src/main.js
@@ -1,109 +1,133 @@
 import "./style.css";
 //import { setupCounter } from "./counter.js";
 
-//Steps to Success :)
-//Array of Music
+//Steps
+//Array of songs
 //Create different categories of the music - In HTML
+//Display all the cards on the screen with two different containers - the overall (songList) and the container for one song (song)
+//Add filter buttons for the different categories/genres
+//Add playlist
+//Add light and dark mode
 
-const music = [
+const songs = [
   {
     name: "Megalovania",
     artist: "Toby Fox",
     genre: "game",
+    image: "",
   },
   {
     name: "Clash Royale Theme Song",
     artist: "Carl V",
     genre: "game",
+    image: "",
   },
   {
     name: "Pigstep",
     artist: "Lena Raine",
     genre: "game",
+    image: "",
   },
   {
     name: "Jumper",
     artist: "Waterflame",
     genre: "game",
+    image: "",
   },
   {
     name: "Lifelight",
     artist: "Hideki Sakamoto",
     genre: "game",
+    image: "",
   },
   {
     name: "Blinding Lights",
     artist: "The Weeknd",
     genre: "pop",
+    image: "",
   },
   {
     name: "Shape of You",
     artist: "Ed Sheeran",
     genre: "pop",
+    image: "",
   },
   {
     name: "Treat You Better",
     artist: "Shawn Mendes",
     genre: "pop",
+    image: "",
   },
   {
-    name: "",
-    artist: "",
-    genre: "",
+    name: "Clarity",
+    artist: "Zedd ft. Foxes",
+    genre: "edm",
+    image: "",
   },
   {
-    name: "",
-    artist: "",
-    genre: "",
+    name: "Animals",
+    artist: "Martin Garrix",
+    genre: "edm",
+    image: "",
   },
   {
-    name: "",
-    artist: "",
-    genre: "",
+    name: "Don't You Worry Child",
+    artist: "Swedish House Mafia",
+    genre: "edm",
+    image: "",
   },
   {
-    name: "",
-    artist: "",
-    genre: "",
+    name: "Silence",
+    artist: "Marshmello ft. Khalid",
+    genre: "edm",
+    image: "",
   },
   {
-    name: "",
-    artist: "",
-    genre: "",
+    name: "Levitating",
+    artist: "Dua Lipa",
+    genre: "Pop",
+    image: "",
   },
   {
-    name: "",
-    artist: "",
-    genre: "",
-  },
-  {
-    name: "",
-    artist: "",
-    genre: "",
-  },
-  {
-    name: "",
-    artist: "",
-    genre: "",
-  },
-  {
-    name: "Smells Like Teen Spirit",
-    artist: "Nirvana",
-    genre: "rock",
-  },
-  {
-    name: "Bohemian Rhapsody",
-    artist: "Queen",
-    genre: "rock",
+    name: "As It Was",
+    artist: "Harry Styles",
+    genre: "Pop",
+    image: "",
   },
   {
     name: "Hotel California",
     artist: "Eagles",
     genre: "rock",
+    image: "",
+  },
+  {
+    name: "Sweet Child O' Mine",
+    artist: "Guns N' Roses",
+    genre: "rock",
+    image: "",
+  },
+  {
+    name: "Smells Like Teen Spirit",
+    artist: "Nirvana",
+    genre: "rock",
+    image: "",
+  },
+  {
+    name: "Bohemian Rhapsody",
+    artist: "Queen",
+    genre: "rock",
+    image: "",
+  },
+  {
+    name: "Shelter",
+    artist: "Porter Robinson & Madeon",
+    genre: "edm",
+    image: "",
   },
   {
     name: "Back in Black",
     artist: "AC/DC",
     genre: "rock",
+    image: "",
   },
 ];
