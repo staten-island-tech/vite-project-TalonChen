6293437d6fe0d8b87b8170cdf760682fa137affb
TalonChen <talonc@nycstudents.net>
2025-11-20 11:00:37 -0800

    o

--- app/src/main.js
+++ app/src/main.js
@@ -5,8 +5,8 @@ import "./style.css";
 //Array of songs
 //Create different categories of the music - In HTML
 //Display all the cards on the screen with two different containers - the overall (songList) and the container for one song (song)
-//Add filter buttons for the different categories/genres
-//Add playlist
+//Create filter buttons for the different categories/genres
+//Create a playlist
 //Add light and dark mode
 
 const songs = [
@@ -14,134 +14,134 @@ const songs = [
     name: "Megalovania",
     artist: "Toby Fox",
     genre: "game",
-    image: "images/megalovania.jpg",
+    image: "src/images/megalovania.jpg",
   },
   {
     name: "Clash Royale Theme Song",
     artist: "Carl V",
     genre: "game",
-    image: "images/clashroyale.jpg",
+    image: "src/images/clashroyale.jpg",
   },
   {
     name: "Pigstep",
     artist: "Lena Raine",
     genre: "game",
-    image: "images/pigstep.jpg",
+    image: "src/images/pigstep.jpg",
   },
   {
     name: "Jumper",
     artist: "Waterflame",
     genre: "game",
-    image: "images/jumper.webp",
+    image: "src/images/jumper.webp",
   },
   {
     name: "Lifelight",
     artist: "Hideki Sakamoto",
     genre: "game",
-    image: "images/lifelight.jpg",
+    image: "src/images/lifelight.jpg",
   },
   {
     name: "Blinding Lights",
     artist: "The Weeknd",
     genre: "pop",
-    image: "images/blindinglights.jpg",
+    image: "src/images/blindinglights.jpg",
   },
   {
     name: "Shape of You",
     artist: "Ed Sheeran",
     genre: "pop",
-    image: "images/shapeofyou.webp",
+    image: "src/images/shapeofyou.webp",
   },
   {
     name: "Treat You Better",
     artist: "Shawn Mendes",
     genre: "pop",
-    image: "images/treatyoubetter.jpg",
+    image: "src/images/treatyoubetter.jpg",
   },
   {
     name: "Clarity",
     artist: "Zedd ft. Foxes",
     genre: "edm",
-    image: "images/clarity.jpg",
+    image: "src/images/clarity.jpg",
   },
   {
     name: "Animals",
     artist: "Martin Garrix",
     genre: "edm",
-    image: "images/animals.jpg",
+    image: "src/images/animals.jpg",
   },
   {
     name: "Don't You Worry Child",
     artist: "Swedish House Mafia",
     genre: "edm",
-    image: "images/dontyouworrychild.jpg",
+    image: "src/images/dontyouworrychild.jpg",
   },
   {
     name: "Silence",
     artist: "Marshmello ft. Khalid",
     genre: "edm",
-    image: "images/silence.webp",
+    image: "src/images/silence.webp",
   },
   {
     name: "Levitating",
     artist: "Dua Lipa",
-    genre: "Pop",
-    image: "images/levitating.webp",
+    genre: "pop",
+    image: "src/images/levitating.webp",
   },
   {
     name: "As It Was",
     artist: "Harry Styles",
-    genre: "Pop",
-    image: "images/asitwas.jpg",
+    genre: "pop",
+    image: "src/images/asitwas.jpg",
   },
   {
     name: "Hotel California",
     artist: "Eagles",
     genre: "rock",
-    image: "images/hotelcalifornia.jpg",
+    image: "src/images/hotelcalifornia.jpg",
   },
   {
     name: "Sweet Child O' Mine",
     artist: "Guns N' Roses",
     genre: "rock",
-    image: "images/sweetchildomine.jpg",
+    image: "src/images/sweetchildomine.jpg",
   },
   {
     name: "Smells Like Teens Spirit",
     artist: "Nirvana",
     genre: "rock",
-    image: "images/smellsliketeensspirit.jpg",
+    image: "src/images/smellsliketeensspirit.jpg",
   },
   {
     name: "Bohemian Rhapsody",
     artist: "Queen",
     genre: "rock",
-    image: "images/bohemianrhapsody.webp",
+    image: "src/images/bohemianrhapsody.webp",
   },
   {
     name: "Shelter",
     artist: "Porter Robinson & Madeon",
     genre: "edm",
-    image: "images/shelter.jpg",
+    image: "src/images/shelter.jpg",
   },
   {
     name: "Back in Black",
     artist: "AC/DC",
     genre: "rock",
-    image: "images/backinblack.jpg",
+    image: "src//backinblack.jpg",
   },
 ];
-const musicstore = document.querySelector(".musicStore");
+const musicStore = document.querySelector(".musicLibrary");
 
 function getSongs(songs) {
-  musicstore.innerHTML = "";
+  musicStore.innerHTML = "";
   songs.forEach((song) => {
-    musicstore.insertAdjacentElement(
+    musicStore.insertAdjacentHTML(
       "afterbegin",
-      `<div class="songs">
+      `<div class="songs" data-genre = "${song.genre}">
         <h2 class = "song">${song.name}</h2>
-        <img src="${song.image}" alt = ${song.name} />
-        <button class = "btn song add">Add to Playlist</button>
+        <img src="${song.image}" alt = "${song.name}" />
+        <button class = "btn song add" data-name = "${song.name}">Add to Playlist</button>
       </div>
       `
     );
